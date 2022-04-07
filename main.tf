52b362d38efe2107675583917d2790d92ad4373c
sedthawut tipkanpirome <sedthawut2000@gmail.com>
2022-04-07 21:25:08 +0700

    (update): add support custome token

--- main.tf
+++ main.tf
@@ -142,11 +142,11 @@ resource "aws_lb_listener_rule" "this" {
   }
 
   dynamic "condition" {
-    for_each = var.custom_header_token == "" ? [] : [var.custom_header_token]
+    for_each = var.custom_header_token == "" ? [] : [true]
     content {
       http_header {
         http_header_name = "custom-header-token" # Match value within cloudfront module
-        values           = [each.value]
+        values           = [var.custom_header_token]
       }
     }
   }
