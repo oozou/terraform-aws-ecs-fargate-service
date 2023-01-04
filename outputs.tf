cc02d059e96fd40027dfae8ba686d29f57b59605
sedthawut tipkanpirome <40098197+xshot9011@users.noreply.github.com>
2023-01-04 13:28:36 +0700

    feat: add output

--- outputs.tf
+++ outputs.tf
@@ -40,5 +40,10 @@ output "target_group_id" {
 
 output "cloudwatch_log_group_name" {
   description = "The name of the log group"
-  value       = try(aws_cloudwatch_log_group.this[0].name, "")
+  value       = try(aws_cloudwatch_log_group.this[0].name, null)
+}
+
+output "cloudwatch_log_group_arn" {
+  description = "The name of the log group"
+  value       = try(aws_cloudwatch_log_group.this[0].arn, null)
 }
