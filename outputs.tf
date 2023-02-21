f488403ac23d0f1422cf00849012687dccf8fb73
xshot9011 <40098197+xshot9011@users.noreply.github.com>
2023-02-21 23:48:24 +0700

    chore: update output

--- outputs.tf
+++ outputs.tf
@@ -1,42 +1,42 @@
-# output "task_role_arn" {
-#   description = "ECS Task role ARN"
-#   value       = local.task_role_arn
-# }
+output "task_role_arn" {
+  description = "ECS Task role ARN"
+  value       = local.task_role_arn
+}
 
-# output "task_execution_role_id" {
-#   description = "ECS Task execution role ID"
-#   value       = local.task_execution_role_id
-# }
+output "task_execution_role_id" {
+  description = "ECS Task execution role ID"
+  value       = local.task_execution_role_id
+}
 
-# output "task_execution_role_arn" {
-#   description = "ECS Task execution role ARN"
-#   value       = local.task_execution_role_arn
-# }
+output "task_execution_role_arn" {
+  description = "ECS Task execution role ARN"
+  value       = local.task_execution_role_arn
+}
 
-# output "secret_arns" {
-#   description = "List of ARNs of the SecretsManager secrets"
-#   value       = try(aws_secretsmanager_secret.service_secrets.arn, "")
-# }
+output "secret_arns" {
+  description = "List of ARNs of the SecretsManager secrets"
+  value       = try(aws_secretsmanager_secret.this[*].arn, "")
+}
 
-# output "target_group_arn" {
-#   description = "id - ARN of the Target Group (matches arn)."
-#   value       = try(aws_lb_target_group.this[0].arn, "")
-# }
+output "target_group_arn" {
+  description = "id - ARN of the Target Group (matches arn)."
+  value       = try(aws_lb_target_group.this[0].arn, "")
+}
 
-# output "target_group_id" {
-#   description = "id - ARN of the Target Group (matches arn)."
-#   value       = try(aws_lb_target_group.this[0].id, "")
-# }
+output "target_group_id" {
+  description = "id - ARN of the Target Group (matches arn)."
+  value       = try(aws_lb_target_group.this[0].id, "")
+}
 
-# output "cloudwatch_log_group_name" {
-#   description = "The name of the log group"
-#   value       = try(aws_cloudwatch_log_group.this[0].name, null)
-# }
+output "cloudwatch_log_group_name" {
+  description = "The name of the log group"
+  value       = try(aws_cloudwatch_log_group.this[0].name, null)
+}
 
-# output "cloudwatch_log_group_arn" {
-#   description = "The name of the log group"
-#   value       = try(aws_cloudwatch_log_group.this[0].arn, null)
-# }
+output "cloudwatch_log_group_arn" {
+  description = "The name of the log group"
+  value       = try(aws_cloudwatch_log_group.this[0].arn, null)
+}
 
 output "debug" {
   description = "x"
