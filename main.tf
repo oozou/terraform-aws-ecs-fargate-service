73c39635aa37cf17bd206f50196020600f6864d3
xshot9011 <40098197+xshot9011@users.noreply.github.com>
2023-05-04 15:44:18 +0700

    chore: test

--- main.tf
+++ main.tf
@@ -115,7 +115,7 @@ resource "aws_cloudwatch_log_group" "this" {
 
   name              = local.log_group_name
   retention_in_days = var.cloudwatch_log_retention_in_days
-  kms_key_id        = local.cloudwatch_log_group_kms_key_arn
+  # kms_key_id        = local.cloudwatch_log_group_kms_key_arn
 
   tags = merge(local.tags, { "Name" = local.log_group_name })
 }
